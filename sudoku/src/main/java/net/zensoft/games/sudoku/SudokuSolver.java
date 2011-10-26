9e7cc9b539723b7c576c4ed09935bf576d0f170b
Agustín Ramos <zentimental.software@gmail.com>
2011-10-26 17:13:22 -0500

    Minor enhancements on SudokuSolver.java

--- sudoku/src/main/java/net/zensoft/games/sudoku/SudokuSolver.java
+++ sudoku/src/main/java/net/zensoft/games/sudoku/SudokuSolver.java
@@ -5,73 +5,121 @@ import static org.hamcrest.Matchers.equalTo;
 
 import java.util.List;
 
+/**
+ * Implements a backtracking algorithm to solve Sudoku games.
+ * 
+ * @author Agustín Ramos
+ */
 public class SudokuSolver {
 
   private Sudoku problem;
-  private Sudoku solution;
 
+  /**
+   * Initializes with a problem to solve.
+   * 
+   * @param problem
+   */
   public SudokuSolver(Sudoku problem) {
     this.problem = problem;
-    this.solution = new Sudoku(problem.asIntArray());
   }
 
+  /**
+   * Go for it!
+   * 
+   * @return a new Sudoku object representing the solution
+   * @throws IllegalArgumentException when it's impossible to solve this sudoku.
+   */
   public Sudoku solve() {
-    if (! solveCells( cellsToSolve() ) ) {
-      throw new IllegalStateException("Impossible to solve this Sudoku!");
+    Sudoku solution = new Sudoku(problem.asIntArray().clone());
+    if (! solveCells(  solution, cellsToSolve( problem ) ) ) {
+      throw new IllegalArgumentException("Impossible to solve this Sudoku!");
     }
     return solution;
   }
 
-  List<CellValue> cellsToSolve() {
+  private List<CellValue> cellsToSolve(Sudoku game) {
     return
       filter(
         having(on(CellValue.class).getValue(), equalTo( -1 )), // Remember, -1 means undefined
-        solution.asCellValueList() );
+        game.asCellValueList() );
   }
   
-  private boolean solveCells(List<CellValue> solutions) {
+  /**
+   * Magic happens here
+   * Given the current state of the board, it tries to find the solution for a list of cells.
+   * @param solutions
+   * @return
+   */
+  private boolean solveCells(Sudoku game, List<CellValue> solutions) {
     if (! solutions.isEmpty() ) {
       
-      resetSolutions(solutions);
-      CellValue first = solutions.get(0);
+      resetSolutions(solutions); // clean slate for this cells
+      CellValue pivot = head(solutions);
       boolean isSublistSolved;
       
       do {
-        if (!solveCell(first))
+        if (!solveCell(game, pivot))
           return false;
-        isSublistSolved = solveCells(subList(solutions));
-      } while (! isSublistSolved && first.hasSolutionCandidates());
+        isSublistSolved = solveCells(game, tail(solutions));
+      } while (! isSublistSolved && pivot.hasSolutionCandidates());
       
       if (!isSublistSolved) {
-        clearCell(first);
+        clearCell(game, pivot);
         return false;
       }
     }
     return true;
   }
 
-  private List<CellValue> subList(List<CellValue> candidates) {
+  /**
+   * Returns the head of a list
+   * @param <T>
+   * @param list
+   * @return
+   */
+  private <T> T head(List<T> list) {
+    return list.get(0);
+  }
+  
+  /**
+   * Returns the tail of a list
+   * @param candidates
+   * @return
+   */
+  private List<CellValue> tail(List<CellValue> candidates) {
     return candidates.subList(1, candidates.size());
   }
 
+  /**
+   * Resets each cell solution as if you haven't tried any value in it.
+   * @param solutions
+   */
   private void resetSolutions(List<CellValue> solutions) {
     forEach(solutions).reset();
   }
   
-  private boolean solveCell(CellValue cell) {
+  /**
+   * Finds a solution for a cell, given the current state of the board.
+   * CellValue.value contains the current guess for the cell, if that
+   * value proves to be wrong, it tryies the nex solution until a valid one
+   * it's found.
+   * @param cell
+   * @return
+   */
+  private boolean solveCell(Sudoku game, CellValue cell) {
     while (cell.hasSolutionCandidates()) {
       cell.nextSolutionCandidate();
-      if (solution.isValidValue( cell )) {
-        solution.setValue( cell );
+      if (game.isValidValue( cell )) {
+        game.setValue( cell );
         return true;
       }
     }
-    clearCell(cell);
+    clearCell(game, cell);
     return false;
   }
   
-  private void clearCell(CellValue cell) {
-    solution.clear(cell.row, cell.col);
+  private void clearCell(Sudoku game, CellValue cell) {
+    game.clear(cell.row, cell.col);
   }
 
 }
