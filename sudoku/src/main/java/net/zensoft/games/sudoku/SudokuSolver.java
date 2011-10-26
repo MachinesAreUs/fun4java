9a7eed216795cce90b8e48295368c6578710d425
Agustín Ramos <zentimental.software@gmail.com>
2011-10-26 17:25:05 -0500

    It actually looks prettier without javadoc ;-)

--- sudoku/src/main/java/net/zensoft/games/sudoku/SudokuSolver.java
+++ sudoku/src/main/java/net/zensoft/games/sudoku/SudokuSolver.java
@@ -12,23 +12,12 @@ import java.util.List;
  */
 public class SudokuSolver {
 
-  private Sudoku problem;
+  final private Sudoku problem;
 
-  /**
-   * Initializes with a problem to solve.
-   * 
-   * @param problem
-   */
   public SudokuSolver(Sudoku problem) {
     this.problem = problem;
   }
 
-  /**
-   * Go for it!
-   * 
-   * @return a new Sudoku object representing the solution
-   * @throws IllegalArgumentException when it's impossible to solve this sudoku.
-   */
   public Sudoku solve() {
     Sudoku solution = new Sudoku(problem.asIntArray().clone());
     if (! solveCells(  solution, cellsToSolve( problem ) ) ) {
@@ -44,12 +33,6 @@ public class SudokuSolver {
         game.asCellValueList() );
   }
   
-  /**
-   * Magic happens here
-   * Given the current state of the board, it tries to find the solution for a list of cells.
-   * @param solutions
-   * @return
-   */
   private boolean solveCells(Sudoku game, List<CellValue> solutions) {
     if (! solutions.isEmpty() ) {
       
@@ -71,41 +54,18 @@ public class SudokuSolver {
     return true;
   }
 
-  /**
-   * Returns the head of a list
-   * @param <T>
-   * @param list
-   * @return
-   */
   private <T> T head(List<T> list) {
     return list.get(0);
   }
   
-  /**
-   * Returns the tail of a list
-   * @param candidates
-   * @return
-   */
   private List<CellValue> tail(List<CellValue> candidates) {
     return candidates.subList(1, candidates.size());
   }
 
-  /**
-   * Resets each cell solution as if you haven't tried any value in it.
-   * @param solutions
-   */
   private void resetSolutions(List<CellValue> solutions) {
     forEach(solutions).reset();
   }
-  
-  /**
-   * Finds a solution for a cell, given the current state of the board.
-   * CellValue.value contains the current guess for the cell, if that
-   * value proves to be wrong, it tryies the nex solution until a valid one
-   * it's found.
-   * @param cell
-   * @return
-   */
+
   private boolean solveCell(Sudoku game, CellValue cell) {
     while (cell.hasSolutionCandidates()) {
       cell.nextSolutionCandidate();
